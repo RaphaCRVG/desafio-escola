fe0d8359fbf9a8dcc6490449bf44f30cbaf48b43
Raphael Oliveira <oliveira.raphael@aluno.senac.rio>
2025-06-09 19:11:50 -0300

    Versão Final

--- src/App.java
+++ src/App.java
@@ -1,10 +1,17 @@
 import models.Aluno;
+import models.Professor;
+import models.Turma;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Aluno aluno = new Aluno("Raphael", "2025001", 20);
-        aluno.setNotaFinal(9.0);
-        aluno.exibirDados();
+        Aluno aluno1 = new Aluno("Raphael", "2025001", 20);
+        aluno1.setNotaFinal(9.0);
+        aluno1.exibirDados();
 
+        Professor professor = new Professor("William","back-end java", 10000.00);
+        professor.exibirDetalhes();
+
+        Turma turma = new Turma("FullStack-2025.1", 1, 19);
+        turma.exibirTurma();
     }
 }
--- src/models/Turma.java
+++ src/models/Turma.java
@@ -1,9 +1,54 @@
 package models;
 
 public class Turma {
-    
+
     // Atributos privados
     private String codigoTurma;
     private int serie;
-    
+    private int quantidadeAlunos = 0;
+
+    // Construtor
+    public Turma(String codigoTurma, int serie, int quantidadeAlunos) {
+        this.codigoTurma = codigoTurma;
+        this.serie = serie;
+        setQuantidadeAlunos(quantidadeAlunos);
+    }
+
+    // Getters e Setters
+    public String getCodigoTurma() {
+        return codigoTurma;
+    }
+
+    public void setCodigoTurma(String codigoTurma) {
+        this.codigoTurma = codigoTurma;
+    }
+
+    public int getSerie() {
+        return serie;
+    }
+
+    public void setSerie(int serie) {
+        this.serie = serie;
+    }
+
+    // Getter e Setter para quantidadeAlunos (Com validação)
+    public int getQuantidadeAlunos() {
+        return quantidadeAlunos;
+    }
+
+    public void setQuantidadeAlunos(int quantidadeAlunos) {
+        if (quantidadeAlunos > 0) {
+            this.quantidadeAlunos = quantidadeAlunos;
+        } else {
+            System.out.println("@ ERROR! @ A quantidade de alunos deve ser maior que 0.");
+        }
+    }
+
+    // Método para exibir detalhes da turma
+    public void exibirTurma() {
+        System.out.println("Código da turma: " + codigoTurma);
+        System.out.println("Série: " + serie);
+        System.out.println("Quantidade de alunos: " + quantidadeAlunos);
+    }
+
 }
